f1187543623ed4e01e3fe39da7627459928e05f6
ArmandoValentin90 <109253210+ArmandoValentin90@users.noreply.github.com>
2022-07-14 10:52:36 -0500

    objetos

--- Backend/src/main/java/com/retoBBVA/consultas/entity/Client.java
+++ Backend/src/main/java/com/retoBBVA/consultas/entity/Client.java
@@ -1,5 +1,20 @@
 package com.retoBBVA.consultas.entity;
 
 public class Client {
+    
+    private String idclient;
+    private String nombre;
+    private String apellidopaterno;
+    private String apellidomaterno;
+    private Date fechadenacimiento;
+    private String sexo;
+    private String segmento;
+    private String nacionalidad;
+    private String rfc;
+    private String tipoID;
+    private Strirng numeroID;
+    private String cuenta;
+    private String email;
 
+    
 }
