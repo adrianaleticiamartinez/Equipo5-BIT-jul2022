f9252b7e168ea19e258791974aa2ba50c49f03a0
Ariel999 <ariel_aaronph@hotmail.com>
2022-07-14 11:00:03 -0500

    Carga Validador.java

--- Backend/src/main/java/com/retoBBVA/consultas/validator/Validator.java
+++ Backend/src/main/java/com/retoBBVA/consultas/validator/Validator.java
@@ -1,5 +1,23 @@
 package com.retoBBVA.consultas.validator;
 
-public class Validator {
+import com.retoBBVA.consultas.entity.User;
 
+public class Validator {
+	
+	
+	public static void login(String username, String password, User usernameDB) {
+		
+		try {
+			if(usernameDB.getUsuario().equals(username)) {
+				if(usernameDB.getAuth().equals(password)) {
+				} else {
+					throw new RuntimeException("Contraseña incorrecta");
+				}
+			} else {
+				throw new RuntimeException("Usuario incorrecto");
+			}
+		} catch (Exception ex) {
+			throw new RuntimeException("Error en funcion login()");
+		}
+	}
 }
